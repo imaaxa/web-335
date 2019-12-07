03f42db27b6c4ef4805ff6e28ad9819cac6119d3
Cory Gilliam <imaaxa@gmail.com>
2019-12-07 00:43:55 -0500

    Exercise 8.2

---
+++ week-8/gilliam_hello_world.py
@@ -0,0 +1,8 @@
+# ===========================================
+# Title: Exercise 8.2
+# Author: Cory Gilliam
+# Date: 9 Dec 2019
+# Modified By:
+# Description: Python hello world
+# ===========================================
+print('Hello World')
